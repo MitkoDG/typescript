572e8f2d1df7a46097d8a23c1eabc7c84122958a
MitkoDG <retimid@abv.bg>
2023-10-03 22:24:30 +0300

    testing the type definition

--- demo.js
+++ demo.js
@@ -4,40 +4,29 @@
 //     }
 // };
 // let courses = null;
-
 // const title = courses?.textFields?.title ?? "No title found";
-
 // logCourseTitle(courses);
-
 // function logCourseTitle(courses){
-
 //     if (!courses?.textFields) {
 //         console.log("textFields not defined");
 //         return;
 //     }
-
 //     if (courses.textFields.title) {
 //         console.log(`The title is: ${courses.textFields.title}`);
 //     }
 // }
-
 // enum DemoCourses {
 //     FREE = "FREE",
 //     PREMIUM = "PREMIUM",
 //     PRIVATE = "PRIVATE",
 //     HIDDEN = "HIDDEN"
 // }
-
 // const courses = {
 //     title: "TypeScript BOOM",
 //     type: DemoCourses.HIDDEN
 // };
-
 // console.log(courses);
-
-let lessonsCount:any = 10; // avoid using any as much as you can or rare
-
-lessonsCount = "Boot"
-
-let lessonsArray = [];
-
+// avoid using any as much as you can or rare
+var lessonsCount = 10;
+var lessonsArray = [];
+console.log(lessonsCount);
