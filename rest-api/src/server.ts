7a7ed1e6fdad6f76fbba20eb4e2a81488acdc7ab
MitkoDG <retimid@abv.bg>
2024-01-02 16:37:57 +0200

    config and run the server

--- rest-api/src/server.ts
+++ rest-api/src/server.ts
@@ -1,2 +1,22 @@
+import * as express from 'express';
+import { root } from './routes/root';
 
-console.log("Hello");
+const app = express();
+
+function setupExpress() {
+
+
+    app.route("/").get(root);
+}
+
+function startServer() {
+
+    app.listen(9000, ()=> {
+        console.log(`HTTP REST Server is now running at http://localhost:9000/`);
+        
+    })
+}
+
+setupExpress();
+
+startServer();
