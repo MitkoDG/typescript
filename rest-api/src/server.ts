45c6043f88ac5c0a64474e7e2e67230fad4cad86
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2024-01-04 13:38:19 +0200

    improving the port configuration
    priority to the env file

--- rest-api/src/server.ts
+++ rest-api/src/server.ts
@@ -1,6 +1,14 @@
+import * as dotenv from "dotenv";
 import * as express from 'express';
 import { root } from './routes/root';
 import { isInteger } from './utils';
+
+const result = dotenv.config();
+
+if (result.error) {
+    console.log(`Error loading enviroment variables, aborting!`);
+    process.exit(1);
+}
 
 const app = express();
 
@@ -14,20 +22,25 @@ function startServer() {
 
     let port: number;
 
-    const portArg = process.argv[2];
+    const portEnv = process.env.PORT,
+        portArg = process.argv[2];
 
-    if (isInteger(portArg)) {
+    if (isInteger(portEnv)) {
+        port = parseInt(portEnv);
+    }
+
+    if (!port && isInteger(portArg)) {
         port = parseInt(portArg);
     }
 
     if (!port) {
         port = 9000;
     }
-    
-    
-    app.listen(port, ()=> {
+
+
+    app.listen(port, () => {
         console.log(`v2 TTP REST Server is now running at http://localhost:${port}/`);
-        
+
     })
 }
 
